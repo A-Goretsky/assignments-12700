d684f40892f824faa141c463a323e656fc773e7d
Anton Goretsky <anton.goretsky@gmail.com>
2017-09-25 07:12:07 -0400

    add picturegrid

---
+++ 04/picturegrid.py
@@ -0,0 +1,19 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+def picturegrid():
+    for y in range(0, len(grid[0])):
+        for x in range(0, len(grid)):
+            if x == 8:
+                print(grid[x][y])
+            else:
+                print(grid[x][y], end='')
+    
+picturegrid()
